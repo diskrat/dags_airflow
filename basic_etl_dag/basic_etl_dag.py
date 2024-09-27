2ac119706c2d8241cc173d2e5c48ffc5a0cf5db6
Joao Silva <joaodoer3@gmail.com>
2024-09-27 14:05:48 -0300

    update bugged

--- basic_etl_dag/basic_etl_dag.py
+++ basic_etl_dag/basic_etl_dag.py
@@ -1,39 +1,46 @@
 import pendulum
 import os
+import pandas as pd
 from airflow.decorators import dag, task
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.operators.bash import BashOperator
 
 conn_id_uri = os.getenv('AIRFLOW_CONN_LOPTESTE')
 default_args = {
-    'start_date': pendulum.naive(2024, 6, 8),
-    'schedule_interval':None,
-    'catchup':False,
+    'start_date': pendulum.naive(2024, 3, 25),
+    'end_date': pendulum.naive(2024, 6, 8),
+    'schedule_interval': None,
+    'catchup': False,
 }
+
 
 @dag(
     dag_id='sql_query',
-    default_args=default_args
+    default_args=default_args,
+    params={
+        'class_id': '8813d508-dc24-47e2-a33f-8d2ca66cab29',
+        'listQuestions_id': '1551e3ed-c875-4bb3-8e4e-58407df40776'}
 )
 def extract_sql():
     @task
-    def extract():
-        sqlquery="""
-            SELECT id FROM lop2teste.submission
+    def extract(logical_=None, **kwargs):
+        sqlquery = """
+            SELECT id FROM submission
             WHERE createdAt  LIKE '{{ ds }}*' AND class_id = '{{params.class_id}}' AND listQuestions_id = '{{params.listQuestions_id}}'
             """
-        
-        class_id='8813d508-dc24-47e2-a33f-8d2ca66cab29'
-        listQuestions_id = '1551e3ed-c875-4bb3-8e4e-58407df40776'
-        query_task = SQLExecuteQueryOperator(
-            task_id='execute_query',
-            conn_id='my_db_conn',
-            sql=sqlquery,
-            params={
-                'class_id':'8813d508-dc24-47e2-a33f-8d2ca66cab29',
-                'listQuestions_id' : '1551e3ed-c875-4bb3-8e4e-58407df40776'
-            }
-        )
-        print(query_task.execute(context={}))
-    extract()
+        mysql_hook = MySqlHook(conn_id='mysql_default')
+        with mysql_hook.get_conn()  as connection:
+            cursor = connection.cursor()
+            cursor.execute(sqlquery)
+            result = cursor.fetchall()
+            columns = [desc[0] for desc in cursor.description]
+            df = pd.DataFrame(result, columns=columns)
+        filename = f"~/airflow/data/{logical_}.csv"
+        df.to_csv(filename, index=False)
+    bashtask = BashOperator(task_id='cria_pasta',
+                            bash_command='mkdir -p ~/airflow/data/')
+    save_extract = extract('{{ds}}')
+    bashtask >> save_extract
+
 
 extract_sql()
--- starter_dag.py
+++ starter_dag.py
@@ -1,20 +1,30 @@
 from airflow.models import DAG
-import pendulum
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.bash import BashOperator
-
+from airflow.decorators import task
+from airflow.utils.dates import days_ago
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 with DAG(
-    'extra_work',
-    start_date=pendulum.today('America/Recife').add(days=-2),
-    schedule_interval='@daily'
-) as dag:
-    tarefa_1 = EmptyOperator(task_id='tarefa1')
-    tarefa_2 = EmptyOperator(task_id='tarefa2')
-    tarefa_3 = EmptyOperator(task_id='tarefa3')
-    tarefa_4 = BashOperator(
-        task_id='cria_pasta',
-        bash_command='mkdir -p /home/joao/docs/airflow/pasta{{data_interval_end}}'
-    )
+    dag_id="test_mysql_query_taskflow",
+    schedule_interval=None,
+    start_date=days_ago(2),
+    catchup=False
     
-    tarefa_1 >> [tarefa_2,tarefa_3]
-    tarefa_3 >> tarefa_4
+) as dag:
+
+    @task
+    def test_mysql_query():
+        # Create a MySqlHook instance
+        # Ensure this matches your connection ID
+        mysql_hook = MySqlHook(conn_id='mysql_default')
+
+        # Define your SQL query
+        sql_query = "SELECT * FROM submission WHERE class_id = '8813d508-dc24-47e2-a33f-8d2ca66cab29' AND listQuestions_id = '{{params.listQuestions_id}}'"
+
+        # Get the DataFrame using get_pandas_df
+        
+        df = mysql_hook.get_pandas_df(sql_query)
+
+        # Display the results
+        print(df)
+
+    # Calling the task
+    test_mysql_query()
